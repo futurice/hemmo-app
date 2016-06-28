1ec7965af9e292e55c379bc3fc59aebf774a6adc
Emmi Siitonen <emppu1994@gmail.com>
2016-06-28 09:38:30 +0300

    Styles.js added

---
+++ src/modules/home/styles.js
@@ -0,0 +1,42 @@
+import {
+  StyleSheet
+} from 'react-native';
+
+const circle = {
+  borderWidth: 0,
+  borderRadius: 60,
+  width: 120,
+  height: 120
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    flexDirection: 'row'
+  },
+  column: {
+    flex: 1,
+    alignItems: 'center',
+    borderWidth: 1
+  },
+  settingsButton: {
+    ...circle,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 20
+  },
+  nameList: {
+    fontSize: 40
+  },
+  button: {
+    color: 'white',
+    fontSize: 40,
+    textAlign: 'center'
+  }
+});
+
+module.exports = styles;
---
+++ src/modules/settings/styles.js
@@ -0,0 +1,79 @@
+import {
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  form: {
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    right: 10,
+    bottom: 10
+  },
+  fieldcolumn: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  buttoncolumn: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  field: {
+    flex: 1,
+    alignItems: 'center',
+    marginBottom: 15
+  },
+  buttonfield: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  label: {
+    textAlign: 'center',
+    fontSize: 20
+  },
+  input: {
+    textAlign: 'center',
+    fontSize: 20
+  },
+  touchable: {
+    borderRadius: 60
+  },
+  highlight: {
+    color: '#F5FCFF'
+  },
+  savebutton: {
+    backgroundColor: 'green',
+    borderRadius: 60,
+    height: 120,
+    width: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  cancelbutton: {
+    backgroundColor: 'grey',
+    borderRadius: 20,
+    height: 30,
+    width: 80,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  imagefield: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  icon: {
+    height: 100,
+    width: 100
+  }
+});
+
+module.exports = styles;
