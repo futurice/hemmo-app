5a1990c1f8422c4f58c6aa0800e54f5e677dadc1
Emmi Siitonen <emppu1994@gmail.com>
2016-07-06 16:01:58 +0300

    Activity Query view updated

--- src/modules/activityquery/ActivityQueryView.js
+++ src/modules/activityquery/ActivityQueryView.js
@@ -1,11 +1,19 @@
 import React, {PropTypes} from 'react';
+import {Map, List} from 'immutable';
 
 import {
-  Text,
   Image,
   View
 } from 'react-native';
 var styles = require('./styles.js');
+
+//TODO: Can paths be formed dynamically? This is ugly and inconvinient
+var activities = List([
+  Map({key: 'Puuhailu', route: require('../../../assets/Puuhailu.jpg'), id: 0}),
+  Map({key: 'Ulkoilu', route: require('../../../assets/Puuhailu.jpg'), id: 1}),
+  Map({key: 'Leikkiminen', route: require('../../../assets/Puuhailu.jpg'), id: 2}),
+  Map({key: 'Yhdessa', route: require('../../../assets/Puuhailu.jpg'), id: 3}),
+  Map({key: 'Lemmikit', route: require('../../../assets/Puuhailu.jpg'), id: 4})]);
 
 const ActivityQueryView = React.createClass({
 
@@ -14,43 +22,31 @@ const ActivityQueryView = React.createClass({
     onNavigate: PropTypes.func.isRequired
   },
   render() {
+    const activityViews = activities.map((activity) => (
+      <View key={activity.get('key')} style={styles.activity}>
+        <Image
+          resizeMode={'contain'}
+          style={styles.activityImage}
+          source={activity.get('route')}/>
+      </View>
+    ));
 
     return (
       <View style={styles.container}>
         <View style={styles.row}>
-          <View style={styles.activity}>
-            <Image resizeMode={'contain'}
-              style={styles.activityImage}
-              source={require('../../../assets/Puuhailu.jpg')}/>
-          </View>
-          <View style={styles.activity}>
-            <Image resizeMode={'contain'}
-              style={styles.activityImage}
-              source={require('../../../assets/Ulkoilu.jpg')}/>
-          </View>
-          <View style={styles.activity}>
-            <Image resizeMode={'contain'}
-              style={styles.activityImage}
-              source={require('../../../assets/Leikkiminen.jpg')}/>
-          </View>
+          {activityViews.get(0)}
+          {activityViews.get(1)}
+          {activityViews.get(2)}
         </View>
 
         <View style={styles.row}>
-          <View style={styles.activity}>
-            <Image resizeMode={'contain'}
-              style={styles.activityImage}
-              source={require('../../../assets/Yhdessa.jpg')}/>
-          </View>
+          {activityViews.get(3)}
           <View style={styles.hemmo}>
             <Image resizeMode={'contain'}
               style={styles.activityImage}
               source={require('../../../assets/Hemmo.jpg')}/>
           </View>
-          <View style={styles.activity}>
-            <Image resizeMode={'contain'}
-              style={styles.activityImage}
-              source={require('../../../assets/Lemmikit.jpg')}/>
-          </View>
+          {activityViews.get(4)}
         </View>
       </View>
     );
