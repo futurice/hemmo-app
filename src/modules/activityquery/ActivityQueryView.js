d43ce980128c358bfb57e9250561f9578e76c08a
Emmi Siitonen <emppu1994@gmail.com>
2016-07-06 15:00:06 +0300

    Activity Query view implemented

--- src/modules/activityquery/ActivityQueryView.js
+++ src/modules/activityquery/ActivityQueryView.js
@@ -2,8 +2,10 @@ import React, {PropTypes} from 'react';
 
 import {
   Text,
+  Image,
   View
 } from 'react-native';
+var styles = require('./styles.js');
 
 const ActivityQueryView = React.createClass({
 
@@ -14,13 +16,46 @@ const ActivityQueryView = React.createClass({
   render() {
 
     return (
-      <View>
-        <Text>
-          Täällä ollaan!
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <View style={styles.activity}>
+            <Image resizeMode={'contain'}
+              style={styles.activityImage}
+              source={require('../../../assets/Puuhailu.jpg')}/>
+          </View>
+          <View style={styles.activity}>
+            <Image resizeMode={'contain'}
+              style={styles.activityImage}
+              source={require('../../../assets/Ulkoilu.jpg')}/>
+          </View>
+          <View style={styles.activity}>
+            <Image resizeMode={'contain'}
+              style={styles.activityImage}
+              source={require('../../../assets/Leikkiminen.jpg')}/>
+          </View>
+        </View>
+
+        <View style={styles.row}>
+          <View style={styles.activity}>
+            <Image resizeMode={'contain'}
+              style={styles.activityImage}
+              source={require('../../../assets/Yhdessa.jpg')}/>
+          </View>
+          <View style={styles.hemmo}>
+            <Image resizeMode={'contain'}
+              style={styles.activityImage}
+              source={require('../../../assets/Hemmo.jpg')}/>
+          </View>
+          <View style={styles.activity}>
+            <Image resizeMode={'contain'}
+              style={styles.activityImage}
+              source={require('../../../assets/Lemmikit.jpg')}/>
+          </View>
+        </View>
       </View>
     );
   }
 });
+
 
 export default ActivityQueryView;
