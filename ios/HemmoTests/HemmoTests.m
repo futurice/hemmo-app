d4dd784eb6104bdaeb2a2e363ce5351856f9b752
Rasmus Eskola <fruitiex@gmail.com>
2016-09-22 12:38:34 +0300

    Rename project to 'Hemmo'

--- ios/HemmoTests/HemmoTests.m
+++ ios/HemmoTests/HemmoTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 240
 #define TEXT_TO_LOOK_FOR @"I'm bored!"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface HemmoTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation HemmoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
