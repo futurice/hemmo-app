e01a638268dd2533ecaf75adb6e729ff24978fa5
Anni Huuhtanen <anni.huuhtanen@gmail.com>
2017-06-07 19:43:30 +0300

    Refactor rendering method in MoodView

--- src/modules/moods/MoodView.js
+++ src/modules/moods/MoodView.js
@@ -12,8 +12,8 @@ import {
   View
 } from 'react-native';
 
-var moods = require('../../data/moods.js');
-var styles = require('./styles.js');
+let moods = require('../../data/moods.js');
+let styles = require('./styles.js');
 
 const MoodView = React.createClass({
 
@@ -38,16 +38,14 @@ const MoodView = React.createClass({
   },
 
   renderBubble(text) {
-    if (this.state.showBubble === true) {
-      return (<SpeechBubble
+    return (
+      <SpeechBubble
         text={text}
         bubbleType={'puhekupla_oikea'}
         hideBubble={this.hideBubble}
-        style={{top: 110, left: 100, margin: 30, fontSize: 14, size: 0.5}}/>);
-    }
-    else {
-      return null;
-    }
+        style={{top: 110, left: 100, margin: 30, fontSize: 14, size: 0.5}}
+      />
+    );
   },
 
   save() {
@@ -55,84 +53,78 @@ const MoodView = React.createClass({
     this.props.dispatch(NavigationState.pushRoute({key: 'Record', allowReturn: true}));
   },
 
-  /* If the mood hasn't been checked yet, it is added to an array what holds the information
-  about the selected moods. If a checked mood is clicked again,
-  it is unchecked and removed from the array*/
   selectMood(mood) {
-    var notSelected = false;
+    let selectedMoods = this.state.selectedMoods;
 
-    for (var j = 0; j < this.state.selectedMoods.size; j++) {
-      if (mood === this.state.selectedMoods.get(j)) {
-        var tmp = this.state.selectedMoods.slice();
-        tmp = tmp.filter(function deleteRoute(item) { return item !== mood; });
-        this.setState({selectedMoods: tmp});
-        notSelected = true;
-      }
-    }
-    /* Mood was not selected, so it is not added to the array */
-    if (notSelected === false) {
-      this.setState({selectedMoods: this.state.selectedMoods.concat(mood)});
-    }
+    // If the mood is already selected, remove it from the list. Otherwise, add it
+    this.setState({
+      selectedMoods: selectedMoods.includes(mood) ? selectedMoods.filter(m => m !== mood) : selectedMoods.concat(mood)
+    });
+  },
+
+  renderMood(mood, key) {
+    return (
+      <TouchableOpacity
+        key={key}
+        style={styles.mood}
+        onPress={this.selectMood.bind(this, mood)}>
+        <Image
+          source={getImage(mood)}
+          style={[
+            getSizeByWidth(mood, 0.17),
+            key === moods.length - 1 ? {marginBottom: getSizeByWidth('ympyra_keski', 0.17).height + 5} : null
+          ]}
+        />
+        {this.state.selectedMoods.includes(mood) ? this.renderCheckmark() : null}
+      </TouchableOpacity>
+    );
+  },
+
+  renderCheckmark() {
+    return (
+      <Image
+        source={getImage('valittu')}
+        style={[styles.check, getSizeByHeight('valittu', 0.1)]}
+      />
+    );
+  },
+
+  renderMoods() {
+    return (
+      <View style={styles.moodColumn}>
+        {moods.map((mood, key) => this.renderMood(mood, key))}
+      </View>
+    );
+  },
+
+  renderHemmo() {
+    return (
+      <View style={styles.hemmo}>
+        <Hemmo
+          key={'hemmo'}
+          image={'hemmo_keski'}
+          size={0.45} restartAudioAndText={this.restartAudioAndText}
+        />
+      </View>
+    );
+  },
+
+  renderNextButton() {
+    return (
+      <TouchableOpacity onPress={this.save} style={styles.saveButton}>
+        <Image source={getImage('nappula_seuraava')} style={getSizeByHeight('nappula_seuraava', 0.1)}/>
+      </TouchableOpacity>
+    );
   },
 
   render() {
-
-    var moodViews = [];
-
-    for (var i = 0; i < moods.length; i++) {
-      var checked = null;
-      for (var j = 0; j < this.state.selectedMoods.size; j++) {
-        if (moods[i] === this.state.selectedMoods.get(j)) {
-          checked = (<Image
-                      source={getImage('valittu')}
-                      style={[styles.check, getSizeByHeight('valittu', 0.1)]}/>);
-        }
-      }
-      var photo;
-
-      if (i === moods.length - 1) {
-        photo = (<Image
-          source={getImage(moods[i])}
-          style={[getSizeByWidth(moods[i], 0.17),
-            {marginBottom: getSizeByWidth('ympyra_keski', 0.17).height + 5}]}/>
-        );
-      }
-      else {
-        photo = (<Image
-          source={getImage(moods[i])}
-          style={[getSizeByWidth(moods[i], 0.17)]}/>);
-      }
-
-      moodViews.push(
-        <TouchableOpacity
-          key={moods[i]}
-          style={[styles.mood]}
-          onPress={this.selectMood.bind(this, moods[i])}>
-            {photo}
-          {checked}
-        </TouchableOpacity>
-      );
-    }
-
-    var speechBubble = this.renderBubble('moods');
-
     return (
       <Image source={getImage('tausta_perus2')} style={styles.container}>
         <Image source={getImage('tausta_levea')} style={styles.moodContainer}>
-          <View style={styles.moodColumn}>
-            {moodViews}
-          </View>
-          <View style={styles.hemmo}>
-            <Hemmo
-              key={'hemmo'}
-              image={'hemmo_keski'}
-              size={0.45} restartAudioAndText={this.restartAudioAndText}
-              />
-          </View>
-          <TouchableOpacity onPress={this.save} style={styles.saveButton}>
-            <Image source={getImage('nappula_seuraava')} style={getSizeByHeight('nappula_seuraava', 0.1)}/>
-          </TouchableOpacity>
-          {speechBubble}
+          {this.renderMoods()}
+          {this.renderHemmo()}
+          {this.renderNextButton()}
+          {this.state.showBubble ? this.renderBubble('moods') : null}
         </Image>
       </Image>
     );
