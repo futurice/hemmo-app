c5154d819366832ff26a6e836ba41cef135606e8
Anni Huuhtanen <anni.huuhtanen@gmail.com>
2017-06-17 12:49:34 +0300

    Refactor AppViewContainer

--- src/modules/AppViewContainer.js
+++ src/modules/AppViewContainer.js
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import {connect} from 'react-redux';
 import {View, StyleSheet, AppState} from 'react-native';
 import NavigationViewContainer from './navigation/NavigationViewContainer';
@@ -7,27 +7,60 @@ import {Map, List} from 'immutable';
 import NavigationModal from '../components/NavigationModal';
 import Spinner from 'react-native-gifted-spinner';
 import * as snapshotUtil from '../utils/snapshot';
-import * as NavigationState from '../modules/navigation/NavigationState';
-import * as SessionState from '../modules/session/SessionState';
+import {resetRoute, pushRoute} from '../modules/navigation/NavigationState';
+import {
+  initializeSessionState,
+  activate,
+  deactivate,
+  resetSessionStateFromSnapshot
+} from '../modules/session/SessionState';
 import store from '../redux/store';
-
 import Orientation from 'react-native-orientation';
 
-const AppViewContainer = React.createClass({
-  propTypes: {
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
+
+const mapStateToProps = state => ({
+  isReady: state.getIn(['session', 'isReady']),
+  currentUser: state.getIn(['user', 'currentUser']),
+  currentPage: state.getIn(['navigationState', 'index']),
+  pages: state.getIn(['navigationState', 'children'])
+});
+
+const mapDispatchToProps = dispatch => ({
+  initializeSessionState: () => dispatch(initializeSessionState()),
+  activate: () => dispatch(activate()),
+  deactivate: () => dispatch(deactivate()),
+  resetSessionStateFromSnapshot: (snapshot) => dispatch(resetSessionStateFromSnapshot(snapshot)),
+  resetRoute: () => dispatch(resetRoute()),
+  pushRoute: (key) => dispatch(pushRoute(key))
+});
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class AppViewContainer extends Component {
+
+  static propTypes = {
     isReady: PropTypes.bool.isRequired,
-    dispatch: PropTypes.func.isRequired,
+    initializeSessionState: PropTypes.func.isRequired,
+    activate: PropTypes.func.isRequired,
+    deactivate: PropTypes.func.isRequired,
+    resetSessionStateFromSnapshot: PropTypes.func.isRequired,
+    resetRoute: PropTypes.func.isRequired,
+    pushRoute: PropTypes.func.isRequired,
     currentUser: PropTypes.instanceOf(Map),
     currentPage: PropTypes.number,
     pages: PropTypes.instanceOf(List)
-  },
+  };
 
-  getInitialState() {
-    return {
-      currentState: AppState.currentState,
-      previousState: null
-    };
-  },
+  state = {
+    currentState: AppState.currentState,
+    previousState: null
+  };
 
   componentDidMount() {
     Orientation.lockToLandscape();
@@ -35,68 +68,58 @@ const AppViewContainer = React.createClass({
 
     /* Haetaan viimeisin tila */
     snapshotUtil.resetSnapshot()
-      .then(snapshot => {
-        const {dispatch} = this.props;
-
-        /* Jos viimeisin tila löytyi */
-        if (snapshot) {
-          dispatch(SessionState.resetSessionStateFromSnapshot(snapshot))
-            .then(() => {
-              this.props.dispatch(SessionState.activate());
-              this.resetRoute();
-            });
-        }
-        /* Ei löytynyt. Aloitetaan alusta */
-        else {
-          dispatch(SessionState.initializeSessionState());
-        }
-
-        /* Tallennetaan uusin tila aina kun statea päivitetään */
-        store.subscribe(() => {
-          snapshotUtil.saveSnapshot(store.getState());
+    .then(snapshot => {
+      /* Jos viimeisin tila löytyi */
+      if (snapshot) {
+        this.props.resetSessionStateFromSnapshot(snapshot)
+        .then(() => {
+          this.props.activate();
+          this.props.resetRoute();
         });
-      });
-  },
+      }
+      /* Ei löytynyt. Aloitetaan alusta */
+      else {
+        this.props.initializeSessionState();
+      }
 
-  _handleAppStateChange(appState) {
+      /* Tallennetaan uusin tila aina kun statea päivitetään */
+      store.subscribe(() => {
+        snapshotUtil.saveSnapshot(store.getState());
+      });
+    });
+  }
+
+  _handleAppStateChange = (appState) => {
     let previous = this.state.currentState;
 
     this.setState({currentState: appState, previousState: previous});
 
     if (this.state.currentState === 'active') {
       Orientation.lockToLandscape();
-      this.props.dispatch(SessionState.activate());
+      this.props.activate();
     }
     else if (this.state.currentState === 'inactive' || this.state.currentState === 'background') {
-      this.props.dispatch(SessionState.deactivate());
+      this.props.deactivate();
     }
-  },
+  };
 
-  resetRoute() {
-    this.props.dispatch(NavigationState.resetRoute());
-  },
-
-  quit() {
-    this.props.dispatch(NavigationState.pushRoute({key: 'End', allowReturn: false}));
-  },
-
-  shouldRenderNavigationModal() {
+  shouldRenderNavigationModal = () => {
     let key = this.props.pages.get(this.props.currentPage).get('key');
 
     return key !== 'Settings' && key !== 'Home' && key !== 'End';
-  },
+  };
 
-  renderNavigationModal() {
+  renderNavigationModal = () => {
     return this.shouldRenderNavigationModal() ? (
       <NavigationModal
-        resetRoute={this.resetRoute}
+        resetRoute={this.props.resetRoute}
         phase={this.props.pages.get(this.props.currentPage - 1).get('key') === 'Moods' ? 'moods' : 'other'}
         shouldSave={this.props.currentPage !== 1}
         currentUser={this.props.currentUser}
-        quit={this.quit}
+        quit={() => this.props.pushRoute({key: 'End', allowReturn: false})}
       />
     ) : null;
-  },
+  };
 
   render() {
     if (!this.props.isReady) {
@@ -114,21 +137,4 @@ const AppViewContainer = React.createClass({
       </View>
     );
   }
-});
-
-const styles = StyleSheet.create({
-  centered: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
-
-export default connect(
-  state => ({
-    isReady: state.getIn(['session', 'isReady']),
-    currentUser: state.getIn(['user', 'currentUser']),
-    currentPage: state.getIn(['navigationState', 'index']),
-    pages: state.getIn(['navigationState', 'children'])
-  })
-)(AppViewContainer);
+}
