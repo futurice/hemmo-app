cbcf0d588f052557132f3a1ce3e458fd846bbaec
Anni Huuhtanen <anni.huuhtanen@gmail.com>
2017-06-17 15:19:42 +0300

    Refactor ThumbVoteViewContainer and TitlePanel

--- src/modules/activity/thumbs/ThumbVoteViewContainer.js
+++ src/modules/activity/thumbs/ThumbVoteViewContainer.js
@@ -1,134 +1,144 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import {connect} from 'react-redux';
 import {List} from 'immutable';
 import TitlePanel from '../../../components/TitlePanel';
 import Hemmo from '../../../components/Hemmo';
-import * as UserState from '../../../modules/user/UserState';
-import * as NavigationState from '../../../modules/navigation/NavigationState';
+import {pushRoute, popRoute} from '../../navigation/NavigationState';
+import {saveAnswer} from '../../user/UserState';
 import SpeechBubble from '../../../components/SpeechBubble';
 import {getSizeByHeight, getSizeByWidth, getImage} from '../../../services/graphics';
-
 import {
   View,
   TouchableOpacity,
   Image
 } from 'react-native';
 
-var styles = require('./styles.js');
-var thumb_values = [
-  {value: 1, text: 'peukku_ylos_0'},
-  {value: 0, text: 'peukku_keski_0'},
-  {value: -1, text: 'peukku_alas_0'}
+let styles = require('./styles.js');
+let thumbs = [
+  {value: 1, imageName: 'peukku_ylos_0'},
+  {value: 0, imageName: 'peukku_keski_0'},
+  {value: -1, imageName: 'peukku_alas_0'}
 ];
 
-//TODO: Separate rendering to smaller components.
-const ThumbVoteViewContainer = React.createClass({
+const mapStateToProps = state => ({
+  savedActivities: state.getIn(['user', 'currentUser', 'answers', 'activities']),
+  activityIndex: state.getIn(['user', 'currentUser', 'activityIndex'])
+});
 
-  propTypes: {
+const mapDispatchToProps = dispatch => ({
+  saveAnswer: (index, destination, answers) => dispatch(saveAnswer(index, destination, answers)),
+  pushRoute: (key) => dispatch(pushRoute(key)),
+  popRoute: () => dispatch(popRoute())
+});
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class ThumbVoteViewContainer extends Component {
+
+  static propTypes = {
+    saveAnswer: PropTypes.func.isRequired,
+    pushRoute: PropTypes.func.isRequired,
+    popRoute: PropTypes.func.isRequired,
     savedActivities: PropTypes.instanceOf(List),
-    dispatch: PropTypes.func.isRequired,
     activityIndex: PropTypes.number.isRequired
-  },
+  };
 
-  getInitialState() {
-    return {
-      showBubble: true
-    };
-  },
+  state = {
+    showBubble: true
+  };
 
-  vote(vote) {
-    this.props.dispatch(
-      UserState.saveAnswer(this.props.activityIndex, 'thumb', vote)
-    );
+  vote = (vote) => {
+    this.props.saveAnswer(this.props.activityIndex, 'thumb', vote);
+    this.props.pushRoute({key: 'Record', allowReturn: true});
+  };
 
-    this.props.dispatch(
-      NavigationState.pushRoute({key: 'Record', allowReturn: true})
-    );
-  },
-
-  hideBubble() {
+  hideBubble = () => {
     this.setState({showBubble: false});
-  },
+  };
 
-  restartAudioAndText() {
+  restartAudioAndText = () => {
     this.setState({showBubble: true});
-  },
+  };
 
-  renderBubble(text, i, j) {
-    if (this.state.showBubble === true) {
-      return (
-        <SpeechBubble
-          text={text}
-          hideBubble={this.hideBubble}
-          bubbleType={'puhekupla_oikea'}
-          style={{top: 100, right: 250, margin: 15, fontSize: 17, size: 0.4}}
-          maIndex={i}
-          saIndex={j}/>
-      );
-    }
-    else {
-      return null;
-    }
-  },
+  renderBubble = (text, i, j) => {
+    return this.state.showBubble ? (
+      <SpeechBubble
+        text={text}
+        hideBubble={this.hideBubble}
+        bubbleType={'puhekupla_oikea'}
+        style={{top: 100, right: 250, margin: 15, fontSize: 17, size: 0.4}}
+        maIndex={i}
+        saIndex={j}
+      />
+    ) : null;
+  };
 
-  renderThumbButtons() {
-    var thumbs = [];
+  renderThumbButton = (thumb, i) => {
+    return (
+      <View key={i}>
+        <TouchableOpacity onPress={() => this.vote(thumb.value)}>
+          <Image
+            source={getImage(thumb.imageName)}
+            style={[styles.voteButton, getSizeByHeight(thumb.imageName, 0.25)]}/>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
-    for (var i = 0; i <= 2; i++) {
-      thumbs.push(
-        <View key={i}>
-          <TouchableOpacity onPress={this.vote.bind(this, thumb_values[i].value)}>
-            <Image
-              source={getImage(thumb_values[i].text)}
-              style={[styles.voteButton, getSizeByHeight(thumb_values[i].text, 0.25)]}/>
-          </TouchableOpacity>
-        </View>
-      );
-    }
-    return thumbs;
-  },
+  renderThumbButtons = () => {
+    return thumbs.map((thumb, i) => this.renderThumbButton(thumb, i));
+  };
 
-  render() {
-
-    var titlePanel = (
+  renderTitlePanel = () => {
+    return (
       <TitlePanel
         activityIndex={this.props.activityIndex}
         savedActivities={this.props.savedActivities}
-        dispatch={this.props.dispatch}/>
+        popRoute={this.props.popRoute}
+      />
     );
+  };
 
-    var thumbs = this.renderThumbButtons();
-
-    var actionPanel = (
+  renderActionPanel = () => {
+    return (
       <View style={styles.actionRow}>
-        {thumbs}
+        {this.renderThumbButtons()}
       </View>
     );
+  };
 
-    var j = this.props.savedActivities.get(this.props.activityIndex).get('sub');
-    var i = this.props.savedActivities.get(this.props.activityIndex).get('main');
-    var speechBubble = this.renderBubble('subActivity', i, j);
+  renderLeftColumn = () => {
+    return (
+      <Image
+        source={getImage('tausta_kapea')}
+        style={[styles.leftColumn, getSizeByWidth('tausta_kapea', 0.6)]}>
+        {this.renderTitlePanel()}
+        {this.renderActionPanel()}
+      </Image>
+    );
+  };
+
+  renderRightColumn = () => {
+    return (
+      <View style={styles.rightColumn}>
+        <Hemmo
+          image={'hemmo_keski'}
+          size={0.7}
+          restartAudioAndText={this.restartAudioAndText}
+        />
+      </View>
+    );
+  };
+
+  render() {
+    let subActivity = this.props.savedActivities.get(this.props.activityIndex).get('sub');
+    let mainActivity = this.props.savedActivities.get(this.props.activityIndex).get('main');
 
     return (
       <Image source={getImage('tausta_perus')} style={styles.container}>
-          <Image
-            source={getImage('tausta_kapea')}
-            style={[styles.leftColumn, getSizeByWidth('tausta_kapea', 0.6)]}>
-              {titlePanel}
-              {actionPanel}
-          </Image>
-        <View style={styles.rightColumn}>
-          <Hemmo image={'hemmo_keski'} size={0.7} restartAudioAndText={this.restartAudioAndText}/>
-        </View>
-        {speechBubble}
+        {this.renderLeftColumn()}
+        {this.renderRightColumn()}
+        {this.renderBubble('subActivity', mainActivity, subActivity)}
       </Image>
     );
   }
-});
-
-export default connect(
-  state => ({
-    savedActivities: state.getIn(['user', 'currentUser', 'answers', 'activities']),
-    activityIndex: state.getIn(['user', 'currentUser', 'activityIndex'])
-  })
-)(ThumbVoteViewContainer);
+}
