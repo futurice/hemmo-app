e89bcf38c1a3e45894357d167f424f9dcdab06dc
Anni Huuhtanen <anni.huuhtanen@gmail.com>
2017-06-22 16:44:26 +0300

    Refactor SpeechBubble and decorate TimerMixin

--- src/components/SpeechBubble.js
+++ src/components/SpeechBubble.js
@@ -3,11 +3,10 @@
  By clicking the bubble audio can be played again.
 */
 
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import AudioPlayerViewContainer from '../modules/audioplayer/AudioPlayerViewContainer';
 import {getSizeByHeight, getImage} from '../services/graphics';
 import TimerMixin from 'react-timer-mixin';
-
 import {
   StyleSheet,
   TouchableOpacity,
@@ -16,110 +15,6 @@ import {
   Image,
   AppState
 } from 'react-native';
-
-var phrases = require('../data/phrases.json');
-var bubbleText;
-var audiotrack;
-
-const SpeechBubble = React.createClass({
-
-  propTypes: {
-    text: PropTypes.string.isRequired,
-    hemmo: PropTypes.object,
-    maIndex: PropTypes.number, // index of the selected main activity
-    saIndex: PropTypes.number, // index of the selected sub activity
-    hideBubble: PropTypes.func.isRequired,
-    bubbleType: PropTypes.string,
-    style: PropTypes.object.isRequired
-  },
-
-  mixins: [TimerMixin],
-
-  getInitialState() {
-    return {
-      currentAppState: AppState.currentState
-    };
-  },
-
-  componentDidMount() {
-    AppState.addEventListener('change', this._handleAppStateChange);
-  },
-
-  componentWillUnmount() {
-    AppState.removeEventListener('change', this._handleAppStateChange);
-  },
-
-  _handleAppStateChange(currentAppState) {
-    this.setState({currentAppState});
-  },
-
-  hideBubble() {
-    this.props.hideBubble();
-  },
-
-  renderBubbleText() {
-    //Text of the speech bubble is related to selected main activity.
-    //maIndex is the index of the selected main activity.
-    if (this.props.maIndex || this.props.maIndex === 0) {
-      //Text of the speech bubble is related to selected sub activity.
-      //saIndex is the index of the selected sub activity.
-      if (this.props.saIndex || this.props.saIndex === 0) {
-        audiotrack = phrases[this.props.text][this.props.maIndex].subTexts[this.props.saIndex].audio;
-        return phrases[this.props.text][this.props.maIndex].subTexts[this.props.saIndex].subText;
-      }
-      else {
-        audiotrack = phrases[this.props.text][this.props.maIndex].audio;
-        return phrases[this.props.text][this.props.maIndex].text;
-      }
-    }
-    else {
-      audiotrack = phrases[this.props.text].audio;
-      return phrases[this.props.text].text;
-    }
-  },
-
-  onEnd() {
-    this.setTimeout(
-      () => { this.props.hideBubble(); },
-      2000
-    );
-  },
-
-  render() {
-    bubbleText = this.renderBubbleText();
-
-    if (this.state.currentAppState !== 'active') {
-      console.log('skipping speech bubble in state: ', this.state.currentAppState);
-      return null;
-    }
-
-    return (
-      <TouchableOpacity style={styles.touchable} onPress={this.hideBubble}>
-        <View
-          style={[styles.bubble, {
-            top: this.props.style.top,
-            right: this.props.style.right,
-            left: this.props.style.left}
-          ]}>
-          <Image
-            source={getImage(this.props.bubbleType)}
-            style={[styles.bubbleText, getSizeByHeight(this.props.bubbleType, this.props.style.size)]}>
-            <Text
-              style={[styles.text, {
-                marginTop: this.props.style.marginTop,
-                margin: this.props.style.margin,
-                fontSize: this.props.style.fontSize
-              }]}>
-              {bubbleText}
-            </Text>
-          </Image>
-          <AudioPlayerViewContainer onEnd={this.onEnd} audioTrack={audiotrack}/>
-        </View>
-        {this.props.hemmo}
-      </TouchableOpacity>
-    );
-  }
-});
 
 const styles = StyleSheet.create({
   touchable: {
@@ -142,4 +37,94 @@ const styles = StyleSheet.create({
   }
 });
 
-export default SpeechBubble;
+let phrases = require('../data/phrases.json');
+let reactMixin = require('react-mixin');
+let audiotrack;
+
+@reactMixin.decorate(TimerMixin)
+export default class SpeechBubble extends Component {
+
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    hemmo: PropTypes.object,
+    maIndex: PropTypes.number, // index of the selected main activity
+    saIndex: PropTypes.number, // index of the selected sub activity
+    hideBubble: PropTypes.func.isRequired,
+    bubbleType: PropTypes.string,
+    style: PropTypes.object.isRequired
+  };
+
+  state = {
+    currentAppState: AppState.currentState
+  };
+
+  componentDidMount() {
+    AppState.addEventListener('change', this._handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleAppStateChange);
+  }
+
+  _handleAppStateChange = (currentAppState) => {
+    this.setState({currentAppState});
+  };
+
+  renderBubbleText = () => {
+    //Text of the speech bubble is related to selected main activity.
+    //maIndex is the index of the selected main activity.
+    if (this.props.maIndex || this.props.maIndex === 0) {
+      //Text of the speech bubble is related to selected sub activity.
+      //saIndex is the index of the selected sub activity.
+      if (this.props.saIndex || this.props.saIndex === 0) {
+        audiotrack = phrases[this.props.text][this.props.maIndex].subTexts[this.props.saIndex].audio;
+        return phrases[this.props.text][this.props.maIndex].subTexts[this.props.saIndex].subText;
+      }
+      else {
+        audiotrack = phrases[this.props.text][this.props.maIndex].audio;
+        return phrases[this.props.text][this.props.maIndex].text;
+      }
+    }
+    else {
+      audiotrack = phrases[this.props.text].audio;
+      return phrases[this.props.text].text;
+    }
+  };
+
+  onEnd = () => {
+    this.setTimeout(this.props.hideBubble, 2000);
+  };
+
+  render() {
+    if (this.state.currentAppState !== 'active') {
+      console.log('skipping speech bubble in state: ', this.state.currentAppState);
+      return null;
+    }
+
+    return (
+      <TouchableOpacity style={styles.touchable} onPress={this.props.hideBubble}>
+        <View
+          style={[styles.bubble, {
+            top: this.props.style.top,
+            right: this.props.style.right,
+            left: this.props.style.left}
+          ]}>
+          <Image
+            source={getImage(this.props.bubbleType)}
+            style={[styles.bubbleText, getSizeByHeight(this.props.bubbleType, this.props.style.size)]}>
+            <Text
+              style={[styles.text, {
+                marginTop: this.props.style.marginTop,
+                margin: this.props.style.margin,
+                fontSize: this.props.style.fontSize
+              }]}>
+              {this.renderBubbleText()}
+            </Text>
+          </Image>
+          <AudioPlayerViewContainer onEnd={this.onEnd} audioTrack={audiotrack}/>
+        </View>
+        {this.props.hemmo}
+      </TouchableOpacity>
+    );
+  }
+}
