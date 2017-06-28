8def097639b81c1b6ae900f06af51d2219167523
Rasmus Eskola <fruitiex@gmail.com>
2017-06-28 11:10:23 +0300

    Fix taking photos

--- android/app/src/main/java/com/hemmo/MainApplication.java
+++ android/app/src/main/java/com/hemmo/MainApplication.java
@@ -3,6 +3,9 @@ package com.hemmo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.github.yamill.orientation.OrientationPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new OrientationPackage(),
+            new ImagePickerPackage()
       );
     }
   };
