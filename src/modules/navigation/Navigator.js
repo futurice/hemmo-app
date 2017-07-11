a96b06f0e208757e3936ad62915346f4bfbf51e6
Anni Huuhtanen <anni.huuhtanen@gmail.com>
2017-07-11 12:00:54 +0300

    Create own navigator for feedback flow and include header in it

--- src/modules/navigation/Navigator.js
+++ src/modules/navigation/Navigator.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CardStackStyleInterpolator from 'react-navigation/src/views/CardStackStyleInterpolator';
-import { StackRouter, addNavigationHelpers } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 import HomeViewContainer from '../home/HomeViewContainer';
 import LoginViewContainer from '../login/LoginViewContainer';
 import SettingsViewContainer from '../settings/SettingsViewContainer';
@@ -11,61 +11,67 @@ import RecordViewContainer from '../activity/record/RecordViewContainer';
 import MoodViewContainer from '../moods/MoodViewContainer';
 import NewRoundContainer from '../NewRoundContainer';
 import EndingViewContainer from '../EndingViewContainer';
+import NavigationModal from '../../components/NavigationModal';
 
-const routes = {
-  Home: { screen: HomeViewContainer },
-  Login: { screen: LoginViewContainer },
-  Settings: { screen: SettingsViewContainer },
-  Activity: { screen: ActivityViewContainer },
-  SubActivity: { screen: SubActivityViewContainer },
-  ThumbVote: { screen: ThumbVoteViewContainer },
-  Record: { screen: RecordViewContainer },
-  Mood: { screen: MoodViewContainer },
-  NewRound: { screen: NewRoundContainer },
-  Ending: { screen: EndingViewContainer },
-};
+const HomeNavigator = StackNavigator(
+  {
+    Home: { screen: HomeViewContainer },
+    Login: { screen: LoginViewContainer },
+    Settings: { screen: SettingsViewContainer },
+  }, {
+    headerMode: 'none',
+    initialRouteName: 'Home',
+    transitionConfig: () => ({
+      screenInterpolator: (sceneProps) => {
+        // Disable the transition animation when resetting to the home screen
+        if (
+          sceneProps.index === 0 &&
+          sceneProps.scene.route.routeName !== 'Home' &&
+          sceneProps.scenes.length > 2
+        ) return null;
 
-const routerConfig = {
-  headerMode: 'none',
-  initialRouteName: 'Home',
-  transitionConfig: () => ({
-    screenInterpolator: (sceneProps) => {
-      // Disable the transition animation when resetting to the home screen
-      if (
-        sceneProps.index === 0 &&
-        sceneProps.scene.route.routeName !== 'Home' &&
-        sceneProps.scenes.length > 2
-      ) return null;
+        // Otherwise, use the usual horizontal animation
+        return CardStackStyleInterpolator.forHorizontal(sceneProps);
+      },
+    }),
+  },
+);
 
-      // Otherwise, use the usual horizontal animation
-      return CardStackStyleInterpolator.forHorizontal(sceneProps);
+const FeedbackNavigator = TabNavigator(
+  {
+    Activity: { screen: ActivityViewContainer },
+    // SubActivity: { screen: SubActivityViewContainer },
+    ThumbVote: { screen: ThumbVoteViewContainer },
+    Mood: { screen: MoodViewContainer },
+    Record: { screen: RecordViewContainer },
+    Ending: { screen: EndingViewContainer },
+    // NewRound: { screen: NewRoundContainer },
+  }, {
+    initialRouteName: 'Activity',
+    swipeEnabled: true,
+    lazy: true,
+    backBehavior: 'none',
+  },
+);
+
+const FeedbackNavigatorWithHeader = StackNavigator(
+  {
+    FeedbackWithHeader: {
+      screen: FeedbackNavigator,
+      navigationOptions: {
+        header: <NavigationModal />,
+      },
     },
-  }),
-};
+  },
+);
 
-const Router = StackRouter(routes, routerConfig);
-
-class AppNavigator extends Component {
-  static router = Router;
-
-  render() {
-    const { state, dispatch } = this.props.navigation;
-    const { routes, index } = state;
-
-    // Figure out what to render based on the navigation state and the router:
-    const Screen = Router.getComponentForState(state);
-
-    // The state of the active child screen can be found at routes[index]
-    let childNavigation = { dispatch, state: routes[index] };
-    // If we want, we can also tinker with the dispatch function here, to limit
-    // or augment our children's actions
-
-    // Assuming our children want the convenience of calling .navigate() and so on,
-    // we should call addNavigationHelpers to augment our navigation prop:
-    childNavigation = addNavigationHelpers(childNavigation);
-
-    return <Screen navigation={childNavigation} />;
-  }
-}
+const AppNavigator = StackNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Feedback: {
+      screen: FeedbackNavigatorWithHeader,
+    },
+  }, { headerMode: 'none', initialRouteName: 'Home' },
+);
 
 export default AppNavigator;
