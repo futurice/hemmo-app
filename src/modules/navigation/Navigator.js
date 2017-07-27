652ea5715cb47bf4becf962462154736332a6bda
Rasmus Eskola <fruitiex@gmail.com>
2017-07-27 14:13:27 +0300

    Move all routes into a single StackNavigator

--- src/modules/navigation/Navigator.js
+++ src/modules/navigation/Navigator.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import CardStackStyleInterpolator from 'react-navigation/src/views/CardStackStyleInterpolator';
-import { StackNavigator, TabNavigator } from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 import FeedbackMenu from './FeedbackMenu';
 import HomeViewContainer from '../HomeViewContainer';
 import LoginViewContainer from '../LoginViewContainer';
@@ -13,80 +12,11 @@ import NavigationModal from './NavigationModal';
 import BackButton from './BackButton';
 import Hemmo from './Hemmo';
 
-const HomeNavigator = StackNavigator(
+const AppNavigator = StackNavigator(
   {
     Home: { screen: HomeViewContainer },
     Login: { screen: LoginViewContainer },
     Settings: { screen: SettingsViewContainer },
-  },
-  {
-    initialRouteName: 'Home',
-    transitionConfig: () => ({
-      screenInterpolator: sceneProps => {
-        // Disable the transition animation when resetting to the home screen
-        if (
-          sceneProps.index === 0 &&
-          sceneProps.scene.route.routeName !== 'Home' &&
-          sceneProps.scenes.length > 2
-        )
-          return null;
-
-        // Otherwise, use the usual horizontal animation
-        return CardStackStyleInterpolator.forHorizontal(sceneProps);
-      },
-    }),
-  },
-);
-
-/*
-// Tab navigator
-const FeedbackNavigator = TabNavigator(
-  {
-    Activity: { screen: ActivityViewContainer },
-    Mood: { screen: MoodViewContainer },
-    FreeWord: { screen: FreeWordViewContainer },
-    Ending: { screen: EndingViewContainer },
-  }, {
-    initialRouteName: 'Activity',
-    swipeEnabled: true,
-    lazy: true,
-    tabBarPosition: 'bottom',
-    backBehavior: 'none',
-    tabBarOptions: {
-      showLabel: false,
-      showIcon: true,
-      inactiveBackgroundColor: '#fff',
-      activeBackgroundColor: '#ccc',
-      iconStyle: {
-        width: 64,
-        height: 64,
-      },
-      style: {
-        backgroundColor: '#fff',
-      },
-    },
-  },
-);
-
-const FeedbackNavigatorWithHeader = StackNavigator(
-  {
-    FeedbackWithHeader: {
-      screen: FeedbackNavigator,
-      navigationOptions: ({ navigation }) => (
-        {
-          headerLeft: <NavigationModal />,
-          headerRight: <Hemmo navigation={navigation} />,
-          headerStyle: { backgroundColor: '#FFFFFF' },
-        }
-      ),
-    },
-  },
-);
-*/
-
-// Stack navigator
-const FeedbackNavigator = StackNavigator(
-  {
     FeedbackMenu: { screen: FeedbackMenu },
     Activity: { screen: ActivityViewContainer },
     Mood: { screen: MoodViewContainer },
@@ -94,31 +24,16 @@ const FeedbackNavigator = StackNavigator(
     Ending: { screen: EndingViewContainer },
   },
   {
-    initialRouteName: 'FeedbackMenu',
-    swipeEnabled: true,
-    headerMode: 'none',
-  },
-);
-
-const FeedbackNavigatorWithHeader = StackNavigator({
-  FeedbackWithHeader: {
-    screen: FeedbackNavigator,
     navigationOptions: ({ navigation }) => ({
-      headerLeft: navigation.state.index !== 0 ? <BackButton /> : null,
+      // Only show navigation modal button in feedback menu screen
+      headerLeft:
+        navigation.state.routeName === 'FeedbackMenu'
+          ? <NavigationModal />
+          : undefined,
       headerRight: <Hemmo navigation={navigation} />,
       headerStyle: { backgroundColor: '#FFFFFF' },
     }),
   },
-});
-
-const AppNavigator = StackNavigator(
-  {
-    Home: { screen: HomeNavigator },
-    Feedback: {
-      screen: FeedbackNavigatorWithHeader,
-    },
-  },
-  { headerMode: 'none', initialRouteName: 'Home' },
 );
 
 export default AppNavigator;
