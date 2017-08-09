ef300503e5e0f6ae9a37c9d8c2f029647fa3392e
Sara Sytela <sara.sytela@futurice.com>
2017-08-09 15:18:33 +0300

    Uncommented a line

--- src/modules/EndingViewContainer.js
+++ src/modules/EndingViewContainer.js
@@ -52,7 +52,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   reset: () => {
-    //    dispatch(resetCurrentUser());
+    dispatch(resetCurrentUser());
   },
   startAgain: () => {
     dispatch(
