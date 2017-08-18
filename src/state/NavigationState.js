16ed8594289b1dbe2373b38e0aa9f9966679d541
Anni Huuhtanen <anni.huuhtanen@gmail.com>
2017-08-18 11:47:29 +0300

    Prevent navigating to same screen twice

--- src/state/NavigationState.js
+++ src/state/NavigationState.js
@@ -5,15 +5,25 @@ import includes from 'lodash/includes';
 import AppNavigator from '../modules/navigation/Navigator';
 
 export default function NavigatorReducer(state, action) {
+  if (state) {
+    const currentRouteName = state.getIn([
+      'routes',
+      state.get('index'),
+      'routeName',
+    ]);
+
+    if (
+      includes(NavigationActions, action.type) &&
+      action.routeName !== currentRouteName
+    ) {
+      return fromJS(
+        AppNavigator.router.getStateForAction(action, state.toJS()),
+      );
+    }
+
+    return state;
+  }
+
   // Initial state
-  if (!state) {
-    return fromJS(AppNavigator.router.getStateForAction(action, state));
-  }
-
-  // Is this a navigation action that we should act upon?
-  if (includes(NavigationActions, action.type)) {
-    return fromJS(AppNavigator.router.getStateForAction(action, state.toJS()));
-  }
-
-  return state;
+  return fromJS(AppNavigator.router.getStateForAction(action, state));
 }
